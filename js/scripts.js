8e27151fab86e8fef40d9233dd4af93544b0e786
Rute Cintra <ruteocintra@gmail.com>
2025-01-09 18:36:46 -0300

    lesson 6

--- js/scripts.js
+++ js/scripts.js
@@ -25,41 +25,65 @@
 // console.log('Tamanho do número do telefone: ' + telefone.length);
 
 // Aula 5
-var num1 = 20;
-var num2 = 3;
+// var num1 = 20;
+// var num2 = 3;
 
-var soma = num1 + num2;
-var subtracao = num1 - num2;
-var divisao = num1 / num2;
-var multiplicacao = num1 * num2;
+// var soma = num1 + num2;
+// var subtracao = num1 - num2;
+// var divisao = num1 / num2;
+// var multiplicacao = num1 * num2;
 
-var media = (num1 + num2) / 2;
-console.log('A média é: ' + media);
+// var media = (num1 + num2) / 2;
+// console.log('A média é: ' + media);
 
-console.log(Math.pow(num1, 4));
+// console.log(Math.pow(num1, 4));
 
 
-var increment = 20;
-increment = increment + 5; // redefinimos o valor dela, para o valor que ela já possuía, mais 5.
-console.log(increment); // O console mostrará 25
+// var increment = 20;
+// increment = increment + 5; // redefinimos o valor dela, para o valor que ela já possuía, mais 5.
+// console.log(increment); // O console mostrará 25
 
-// esta estrutura variável = variável + valor, pode ser simplificada com o operador incremental +=
+// // esta estrutura variável = variável + valor, pode ser simplificada com o operador incremental +=
 
-increment = 1;
-increment += 5;
-console.log(increment); // O console mostrará 6
+// increment = 1;
+// increment += 5;
+// console.log(increment); // O console mostrará 6
 
-//conversão de variável
-var telefone = 998887655;
-var ddd = 21;
+// //conversão de variável
+// var telefone = 998887655;
+// var ddd = 21;
 
-var ddd_string = ddd.toString();
-var tel_string = telefone.toString();
+// var ddd_string = ddd.toString();
+// var tel_string = telefone.toString();
 
-console.log (ddd_string + '-' + tel_string);
+// console.log (ddd_string + '-' + tel_string);
 
-var idade = '17';
+// var idade = '17';
 
-idade = parseInt(idade);
-idade++;
-console.log(idade);
+// idade = parseInt(idade);
+// idade++;
+// console.log(idade);
+
+
+// Aula 6
+
+var idade = "65";
+var teste = idade == 60;
+
+console.log(teste); //false
+
+var teste = idade === 65;
+
+console.log(teste); //false
+//igual 3 vezes verifica valor e TIPO
+
+
+var teste = idade != 65;
+
+console.log(teste); //false
+
+var vtype = true;
+
+//tipo de variável
+console.log(typeof vtype == "boolean"); //true
+
