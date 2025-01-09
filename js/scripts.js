468af06dceedcd811415a95d771859b3bfbe8ddb
Rute Cintra <ruteocintra@gmail.com>
2025-01-09 17:33:01 -0300

    lesson 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
-var latitude = 40.87663;
-var longitude = -8.08373;
+// Aula 3
+// var latitude = 40.87663;
+// var longitude = -8.08373;
 
-console.log(latitude);
-console.log(longitude);
+// console.log(latitude);
+// console.log(longitude);
+
+// Aula 4
+var nome = "João";  // podem ser usadas aspas duplas
+var sobrenome = 'Gomes';  // ou aspas simples
+var cpf = '111.111.111-11';  
+var telefone = "998887655";
+var ddd = '21';
+var email = 'joao@gmail.com';
+var biografia = "João começou a sua carreira como desenvolvedor web em 2010 e já participou de importantes projetos, como...";
+
+//concatenar
+console.log('Nome completo: ' + nome + '' + sobrenome);
+console.log (ddd + '-' + telefone);
+
+//posição de caracteres e tamanho da string
+console.log('Primeira letra do nome: ' + nome[0]);
+console.log('Última letra do nome: ' + nome[3]);
+
+console.log('Tamanho do número do telefone: ' + telefone.length);
