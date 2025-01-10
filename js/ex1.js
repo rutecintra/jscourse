e7012d0d7c1bbebfc8234736c14816173f8d72b3
Rute Cintra <ruteocintra@gmail.com>
2025-01-09 21:24:48 -0300

    ex 1

---
+++ js/ex1.js
@@ -0,0 +1,50 @@
+console.log('O arquivo está funcionando');
+
+var nome = 'Rute';
+var idade = '27';
+
+console.log('Olá, eu me chamo ' + nome + ', tenho ' + idade + ' anos e estou estudando Javascript');
+
+var nomealuno = 'Leticya Gostosa';
+var matricula = '08470318454';
+var nota1 = 10;
+var nota2 = 9;
+
+console.log('O aluno ' + nomealuno + ', matrícula ' + matricula + ', obteve a média final ' + (nota1 + nota2) / 2);
+
+var celular = "99602407";
+
+if (celular.length == 9) {
+
+    console.log('Resultado do teste: true');
+
+} else {
+
+    console.log('"Resultado do teste: false".');
+}
+
+console.log('Resultado da operação 32^6: ' + Math.pow(32, 6));
+
+//6: c, f, g
+
+var quantidade = "25";
+var numero = 6;
+var pressao;
+var temperatura = 40;
+temperatura = null;
+
+console.log(quantidade += quantidade);
+console.log( (7+5) / numero + 2 ); 
+console.log(pressao);
+console.log(temperatura);
+console.log(typeof pressao);
+console.log(typeof temperatura);
+
+var idade = 65;
+
+console.log(idade != 65);
+console.log(idade >= 65); 
+console.log("65" == idade);
+console.log(65 !== idade);
+console.log(typeof (idade > 60));
+console.log(typeof (idade > 70));
