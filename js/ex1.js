c14db5f8f7d698107d51e4d48b79ce59dc230032
Rute Cintra <ruteocintra@gmail.com>
2025-01-09 21:31:17 -0300

    ex 1 correction

--- js/ex1.js
+++ js/ex1.js
@@ -1,9 +1,20 @@
+console.log('Questão 1');
+
 console.log('O arquivo está funcionando');
 
+console.log('----------------');
+
+console.log('Questão 2');
+
 var nome = 'Rute';
-var idade = '27';
+var ano = '1997';
+var idade = new Date().getFullYear() - ano;
 
 console.log('Olá, eu me chamo ' + nome + ', tenho ' + idade + ' anos e estou estudando Javascript');
+
+console.log('----------------');
+
+console.log('Questão 3');
 
 var nomealuno = 'Leticya Gostosa';
 var matricula = '08470318454';
@@ -12,20 +23,29 @@ var nota2 = 9;
 
 console.log('O aluno ' + nomealuno + ', matrícula ' + matricula + ', obteve a média final ' + (nota1 + nota2) / 2);
 
+console.log('----------------');
+
+console.log('Questão 4');
+
 var celular = "99602407";
 
-if (celular.length == 9) {
+console.log('"Resultado do teste: "' + celular.length == 9);
 
-    console.log('Resultado do teste: true');
+console.log('----------------');
 
-} else {
-
-    console.log('"Resultado do teste: false".');
-}
+console.log('Questão 5');
 
 console.log('Resultado da operação 32^6: ' + Math.pow(32, 6));
 
-//6: c, f, g
+console.log('----------------');
+
+console.log('Questão 6');
+
+console.log('Opções: c, f, g');
+
+console.log('----------------');
+
+console.log('Questão 7');
 
 var quantidade = "25";
 var numero = 6;
@@ -39,6 +59,10 @@ console.log(pressao);
 console.log(temperatura);
 console.log(typeof pressao);
 console.log(typeof temperatura);
+
+console.log('----------------');
+
+console.log('Questão 8');
 
 var idade = 65;
 
