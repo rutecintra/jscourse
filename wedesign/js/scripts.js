82daf9f0a57d44ef94a0b6fd6ea1a2d0958e3a91
Rute Cintra <ruteocintra@gmail.com>
2025-01-26 18:26:23 -0300

    date footer

--- wedesign/js/scripts.js
+++ wedesign/js/scripts.js
@@ -80,10 +80,7 @@ document.getElementById("service-next").onclick = function() {
 
 // Data Footer
 
+var ano_atual = new Date;
+ano_atual = ano_atual.getFullYear();
 
-
-  
-   
-
-
-   
+document.getElementById("current_year").innerHTML = ano_atual;
