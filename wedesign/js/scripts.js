1099c128b81eacafe48fd319c611c71623a0e11d
Rute Cintra <ruteocintra@gmail.com>
2025-01-26 15:50:26 -0300

    menu mobile

--- wedesign/js/scripts.js
+++ wedesign/js/scripts.js
@@ -1,8 +1,13 @@
 
 // Menu Mobile
 
+document.getElementById("hamburguer-icon").onclick = function() {
+  document.getElementById("sliding-header-menu-outer").style.right = "0px";
+}
 
-
+document.getElementById("sliding-header-menu-close-button").onclick = function() {
+  document.getElementById("sliding-header-menu-outer").style.right = "-320px";
+}
 
 // About us Tab
 
